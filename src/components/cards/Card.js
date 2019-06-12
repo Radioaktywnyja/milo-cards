87b1c66848ee239ed453e2e32cae7cd4853088dd
Radioaktywnyja <milosz.kusak@gmail.com>
2019-06-12 22:43:45 +0200

    Sprawdzanie kart w memory - sprawdzenie czy jedna karta nie została kliknięta 2 razy - zapisanie pierwszej karty do cardA - zapisanie drugiej karty do cardB - porównanie cardA z cardB - w razie pozytywnego porównania usunięcie kart z gry i zwiększenie countera - po osiągnięciu przez counter liczby kart w grze ogłoszenie zwycięstwa

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 import cardback from './../../assets/images/cardback.png';
+import emptycard from './../../assets/images/null.png';
 
 function Card(props) {
 
   function turnCard() {
-    props.dispatch({type: 'TURN', id: props.id});
+    if (props.checked === false && props.id !== props.cardA.id && props.checker < 2) {
+      props.dispatch({type: 'TURN', id: props.id});
+      if (props.checker === 0) {
+        props.dispatch({type: 'CHECK_FIRST', id: props.id, code: props.code});
+      } else if (props.checker === 1) {
+        props.dispatch({type: 'CHECK_SECOND', id: props.id, code: props.code});
+        // props.dispatch({type: 'COMPARE', cardA: props.cardA, cardB: props.cardB});
+      }
+    }
   }
 
   return(
-    <span className="card"><img src={props.complete === false ? cardback : props.image} alt={props.code} className="card" id={props.id} onClick={turnCard} /></span>
+    <span className="card"><img src={props.checked === true ? emptycard : props.complete === false ? cardback : props.image} alt={props.code} className="card" id={props.id} onClick={turnCard} /></span>
   );
 }
 
--- src/components/cards/DealCards.js
+++ src/components/cards/DealCards.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useReducer } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import uuid from 'uuid/v4';
 
 import Card from './Card';
@@ -6,26 +6,36 @@ import Card from './Card';
 const cardReducer = (state, action) => {
   switch (action.type) {
     case 'DUPLICATE':
-      return state.concat(
+      return {
+        ...state,
+        deck: state.deck.concat(
         {
           id: uuid(),
           code: action.code,
           image: action.image,
           complete: false,
+          checked: false
         },
         {
           id: uuid(),
           code: action.code,
           image: action.image,
           complete: false,
+          checked: false
         }
-      );
+      ),
+    }
     case 'SHUFFLE':
-      return state.concat().sort(() => Math.random() - 0.5);
+      return {
+        ...state,
+        deck: state.deck.concat().sort(() => Math.random() - 0.5)
+      }
     case 'TURN':
-      return state.map(card => {
+      return {
+        ...state,
+        deck: state.deck.map(card => {
         if (card.id === action.id) {
-          if (card.complete === false) {
+          if (card.complete === false && !card.checked) {
             return {...card, complete: true};
           } else {
             return {...card, complete: false};
@@ -33,7 +43,37 @@ const cardReducer = (state, action) => {
         } else {
           return card;
         }
-      });
+      })
+    }
+    case 'CHECK_FIRST':
+      return {
+        ...state,
+        checker: state.checker = 1,
+        cardA: { code: action.code, id: action.id }
+      }
+    case 'CHECK_SECOND':
+      return {
+        ...state,
+        checker: state.checker = 2,
+        cardB: { code: action.code, id: action.id },
+      }
+    case 'COMPARE':
+      return {
+        ...state,
+        deck: state.deck.map(card => {
+          if (state.cardA.code === state.cardB.code && (card.id === action.cardA.id || card.id === action.cardB.id)) {
+            return {...card, checked: true};
+          } else if (card.id === action.cardA.id || card.id === action.cardB.id) {
+            return {...card, complete: false};
+          } else {
+            return card;
+          }
+        }),
+        checker: 0,
+        cardA: { id: 0 },
+        cardB: { id: 1 },
+        counter: state.cardA.code === state.cardB.code ? state.counter + 2 : state.counter
+      }
     default:
       throw new Error();
   }
@@ -41,9 +81,9 @@ const cardReducer = (state, action) => {
 
 function DealCards(props) {
 
-  const [cards, dispatchCards] = useReducer(cardReducer, []);
+  const [cards, dispatchCards] = useReducer(cardReducer, { deck: [], checker: 0, counter: 0, cardA: {code: 'cardA'}, cardB: {code: 'cardB'} });
 
-  React.useEffect(() => {
+  useEffect(() => {
     // duplicate drawed cards
     props.cards.map(item => (
        dispatchCards({ type: 'DUPLICATE', code: item.code, image: item.image })
@@ -52,11 +92,26 @@ function DealCards(props) {
     dispatchCards({ type: 'SHUFFLE'})
   }, [props.cards]);
 
+  useEffect(() => {
+    if (cards.checker === 2) {
+      setTimeout(() => {dispatchCards({type: 'COMPARE', cardA: cards.cardA, cardB: cards.cardB})}, 500);
+    }
+  }, [cards.checker]);
+
+  const deckLength = cards.deck.length;
+  useEffect(() => {
+    if (cards.counter === deckLength) {
+      console.log("victory");
+    }
+  }, [cards.counter, deckLength]);
+
   return (
       <Fragment>
-        {cards.map((item) => (
-          <Card code={item.code} image={item.image} key={item.id} id={item.id} complete={item.complete} dispatch={dispatchCards} />
+        {cards.deck.map((item) => (
+          <Card code={item.code} image={item.image} key={item.id} id={item.id} complete={item.complete} checked={item.checked} dispatch={dispatchCards} checker={cards.checker} cardA={cards.cardA} cardB={cards.cardB} />
         ))}
+        {console.log(cards)}
+        {cards.counter}
       </Fragment>
   );
 }
